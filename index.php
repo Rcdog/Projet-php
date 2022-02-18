eba8dd4d14cf58b685134e95778479d25a25dbec
Ghulam Rachid <rachid.93200@hotmail.fr>
2022-02-18 10:27:55 +0100

    ajout home nav et lorem

--- index.php
+++ index.php
@@ -5,3 +5,15 @@ $conn = new mysqli('localhost', 'root', 'root');
 
 if ($conn->connect_error)
     die(sprintf('Imposible de se connecter a la base de donnée'));
+
+SimpleOrm::useConnection($conn, 'projet_php');
+
+if (!empty($_GET['route'])) $route = $_GET['route'];
+
+else $route = 'home';
+
+switch ($route) {
+    case 'home':
+        include __DIR__ . '/controllers/home-controller.php';
+        break;
+}
---
+++ views/home.php
@@ -0,0 +1,12 @@
+<?php include __DIR__ . '/parties/header.php'; ?>
+
+<div class="jumbotron">
+    <h1 class="display-3">Bienvenue sur mon blog</h1>
+    <p class="lead">Blablabla</p>
+    <hr class="my-2">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ex corrupti voluptates nobis sint ratione debitis eveniet doloribus sed, nemo autem eum commodi in possimus sapiente nulla at sunt harum.</p>
+
+</div>
+
+
+<?php include __DIR__ . '/parties/footer.php'; ?>
